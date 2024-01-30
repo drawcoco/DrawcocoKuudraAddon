b474489f62beacdd453c052f77f1856624269d56
drawcoco <corentin216@gmail.com>
2024-01-30 19:21:33 +0100

    empty gui handling
    empty gui functions, ready to be used

--- index.js
+++ index.js
@@ -2,9 +2,44 @@
 import {data} from "./utils"
 import settings from "./settings"
 
+var movedisplay = new Gui();
 
 // option settings GUI
 register("command", () => {
      
 }).setName("DrawcocoKuudraAddon").setAliases("dka");
 
+
+
+
+//===========================================================================================
+//              GUI
+//===========================================================================================
+
+// move graph event
+register("command", () => {
+    movedisplay.open()
+}).setName("moveKuudraGui").setAliases("mkg");
+
+
+// mouse click event
+register("guimouseclick", (x, y, button, gui, event) => {
+
+})
+
+// mouse drag event
+register("dragged", (dx, dy, x, y) => {
+
+});
+
+
+// mouse release event
+register("guimouserelease", (x, y, button, gui, event) => {
+
+});
+
+
+// render event
+register("renderoverlay", () => {
+
+});
