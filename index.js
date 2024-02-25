8344b4ccc745240336638683a98dd972cb29d932
drawcoco <corentin216@gmail.com>
2024-02-25 11:10:02 +0100

    Get Armor Price
    Added Attribute Level Handling

--- index.js
+++ index.js
@@ -50,54 +50,56 @@ register("renderoverlay", () => {
 
 
 
-EndedKuudra = 1;
 
-function getPrice(Type, Part, attr1, attr2) {
+function getPrice(Type, Part, attr1, lvl1, attr2, lvl2) {
     if (Type == "Aurora") {
-      return Math.max(isAuroraGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, attr2)).toString();
+      return Math.max(isAuroraGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else if (Type == "Crimson") {
-      return Math.max(isCrimsonGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, attr2)).toString();
+      return Math.max(isCrimsonGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else if (Type == "Terror") {
-      return Math.max(isTerrorGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, attr2)).toString();
+      return Math.max(isTerrorGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else if (Type == "Hollow") {
-      return Math.max(isHollowGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, attr2)).toString();
+      return Math.max(isHollowGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else { // fervor
-      return uniqueAttr(attr1, attr2).toString();
+      return uniqueAttr(attr1, lvl1, attr2, lvl2).toString();
     }
 }
   
-function uniqueAttr(attr1, attr2) {
+function uniqueAttr(attr1, lvl1, attr2, lvl2) {
+    bonusLvl1 = lvl1 == 6 ? 0.5 : Math.pow(2, 5-lvl1);
+    bonusLvl2 = lvl2 == 6 ? 0.5 : Math.pow(2, 5-lvl2);
+
     prices = [500000];
     if (attr1 == "Mana Pool" || attr2 == "Mana Pool") {
-        prices.push(8000000);
+        prices.push(8000000 / (attr1 == "Mana Pool" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Mana Regeneration" || attr2 == "Mana Regeneration") {
-        prices.push(1000000);
+        prices.push(1000000 / (attr1 == "Mana Regeneration" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Speed" || attr2 == "Speed") {
-        prices.push(1000000);
+        prices.push(1000000 / (attr1 == "Speed" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Veteran" || attr2 == "Veteran") {
-        prices.push(6000000);
+        prices.push(6000000 / (attr1 == "Veteran" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Vitality" || attr2 == "Vitality") {
-        prices.push(5000000);
+        prices.push(5000000 / (attr1 == "Vitality" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Breeze" || attr2 == "Breeze") {
-        prices.push(4000000);
+        prices.push(4000000 / (attr1 == "Breeze" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Dominance" || attr2 == "Dominance") {
-        prices.push(4000000);
+        prices.push(4000000 / (attr1 == "Dominance" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Lifeline" || attr2 == "Lifeline") {
-        prices.push(7000000);
+        prices.push(7000000 / (attr1 == "Lifeline" ? bonusLvl1 : bonusLvl2));
     }
     if (attr1 == "Magic Find" || attr2 == "Magic Find") {
-        prices.push(7000000);
+        prices.push(7000000 / (attr1 == "Magic Find" ? bonusLvl1 : bonusLvl2));
     }
     return Math.max(...prices);
 }
@@ -145,13 +147,53 @@ function isHollowGodRoll(attr1, attr2) {
     return 0;
 }
 
+
+EndedKuudra = 1;
+
 armorTypes = ["Aurora", "Crimson", "Terror", "Fervor", "Hollow"];
 armorParts = ["Helmet", "Chestplate", "Leggings", "Boots"];
 armorAttr = ["Arachno Resistance", "Blazing Resistance", "Breeze", "Dominance","Ender Resistance","Experience","Fortitude","Life Regeneration",
     "Lifeline","Magic Find","Mana Pool","Mana Regeneration","Speed","Undead Resistance","Veteran", "Vitality"];
 bookEnchants = ["Fatal Tempo", "Inferno", "Hardened Mana", "Ferocious Mana", "Strong Mana", "Mana Vampire"];
 
+const romanVal = {
+    I: 1,
+    V: 5,
+    X: 10
+}
 
+
+
+/*
+t5
+fero 1.5
+vamp 1.7
+hard 0.5
+strong 1.8
+
+
+*/
+
+
+
+
+function romanToInt(romanString) {
+    total = 0;
+    for (let i = 0; i < romanString.length; i++) {
+        if (romanString[i] == "I" && romanString[i+1] == "V") {
+            total += 4;
+            i++;
+        }
+        else if (romanString[i] == "I" && romanString[i+1] == "X") {
+            total += 9;
+            i++;
+        }
+        else {
+            total += romanVal[romanString[i]];
+        }
+    }
+    return total;
+}
 
 register("tick", () => {
     try {
@@ -159,7 +201,7 @@ register("tick", () => {
         if (EndedKuudra == 1) {
             openedChest = Player.getContainer();
             // when you open a large chest
-            if (openedChest.getName() == "Large Chest") {
+            if (openedChest.getName() == "Paid Chest" || openedChest.getName() == "Large Chest") {
                 items = openedChest.getItems();
                 // cycle throuth the slots
                 for (i = 0; i < 54; i++) {
@@ -177,7 +219,6 @@ register("tick", () => {
                                     spaces = 0;
                                     // sneaky move to not get stats as attributes (like Speed and Health Regen)
                                     attributes = []
-
                                     items[i].getLore().forEach(lorePart => {
                                         if (lorePart.includes("⚔")) {
                                             spaces += 1;
@@ -185,14 +226,18 @@ register("tick", () => {
                                         // get the attributes and display them
                                         if (spaces == 1) {
                                             armorAttr.forEach(attr => {
-                                                if (lorePart.includes(attr)) {
-                                                    ChatLib.chat(attr);
-                                                    attributes.push(attr);
+                                                if (lorePart.includes(attr) && !lorePart.includes("Grants")) {
+
+                                                    splits = lorePart.split(' ');
+                                                    level = romanToInt(splits[splits.length-1]);
+
+                                                    attributes.push([attr, level]);
+                                                    ChatLib.chat(attr + " &6" + level);
                                                 }
                                             });
                                         }
                                     });
-                                    ChatLib.chat("price: " + getPrice(armorType, armorPart, attributes[0], attributes[1]));
+                                    ChatLib.chat("price: " + getPrice(armorType, armorPart, attributes[0][0], attributes[0][1], attributes[1][0], attributes[1][1]));
                                 }
                             });
                         });
@@ -209,8 +254,13 @@ register("tick", () => {
     }
 });
 
+// debug purpose
+register("command", () => {
+    EndedKuudra = true;
+}).setName("endKuudra");
 
+
+// kuudra end run detection
 register("chat", () => {
-    ChatLib.chat("TRIGGERED KUUDRA ENDED");
     EndedKuudra = true;
 }).setChatCriteria("                        Percentage Complete: 100%");
