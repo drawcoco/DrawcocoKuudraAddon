c20ce4b1d1369777cbf9ed4ebc64c3337ee09eca
drawcoco <corentin216@gmail.com>
2024-02-26 18:40:12 +0100

    Get Equipment Price
    Get equipment price logic added

--- index.js
+++ index.js
@@ -56,29 +56,29 @@ register("renderoverlay", () => {
 
 
 
-function getPrice(Type, Part, attr1, lvl1, attr2, lvl2) {
+function getArmorPrice(Type, Part, attr1, lvl1, attr2, lvl2) {
     if (Type == "Aurora") {
-      return Math.max(isAuroraGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
+      return Math.max(isAuroraGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueArmorAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else if (Type == "Crimson") {
-      return Math.max(isCrimsonGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
+      return Math.max(isCrimsonGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueArmorAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else if (Type == "Terror") {
-      return Math.max(isTerrorGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
+      return Math.max(isTerrorGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueArmorAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else if (Type == "Hollow") {
-      return Math.max(isHollowGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueAttr(attr1, lvl1, attr2, lvl2)).toString();
+      return Math.max(isHollowGodRoll(attr1, attr2) / (Part == "Helmet" ? 10 : 1), uniqueArmorAttr(attr1, lvl1, attr2, lvl2)).toString();
     }
     else { // fervor
-      return uniqueAttr(attr1, lvl1, attr2, lvl2).toString();
+      return uniqueArmorAttr(attr1, lvl1, attr2, lvl2).toString();
     }
 }
   
-function uniqueAttr(attr1, lvl1, attr2, lvl2) {
-    bonusLvl1 = lvl1 == 6 ? 0.5 : Math.pow(2, 5-lvl1);
-    bonusLvl2 = lvl2 == 6 ? 0.5 : Math.pow(2, 5-lvl2);
+function uniqueArmorAttr(attr1, lvl1, attr2, lvl2) {
+    let bonusLvl1 = lvl1 == 6 ? 0.5 : Math.pow(2, 5-lvl1);
+    let bonusLvl2 = lvl2 == 6 ? 0.5 : Math.pow(2, 5-lvl2);
 
-    prices = [500000];
+    let prices = [500000];
     if (attr1 == "Mana Pool" || attr2 == "Mana Pool") {
         prices.push(8000000 / (attr1 == "Mana Pool" ? bonusLvl1 : bonusLvl2));
     }
@@ -152,6 +152,79 @@ function isHollowGodRoll(attr1, attr2) {
     return 0;
 }
 
+function getEquipmentPrice(Part, attr1, lvl1, attr2, lvl2) {
+    return Math.max(isEquipGodRoll(Part, attr1, attr2), uniqueEquipmentAttr(Part, attr1, lvl1, attr2, lvl2));
+}
+
+function isEquipGodRoll(Part, attr1, attr2) {
+    if (Part == "Necklace") {
+        if (attr1 == "Mana Pool" && attr2 == "Mana Regeneration") {
+            return 60000000;
+        }
+    } else if (Part == "Cloak") {
+        if (attr1 == "Lifeline" && attr2 == "Mana Pool") {
+            return 70000000;
+        }
+        if (attr1 == "Magic Find" && attr2 == "Veteran") {
+            return 50000000;
+        }
+        if (attr1 == "Mana Pool" && attr2 == "Mana Regeneration") {
+            return 50000000;
+        }
+    } else if (Part == "Belt") {
+        if (attr1 == "Lifeline" && attr2 == "Mana Pool") {
+            return 90000000;
+        }
+        if (attr1 == "Magic Find" && attr2 == "Veteran") {
+            return 60000000;
+        }
+    } else { // bracelet
+        if (attr1 == "Lifeline" && attr2 == "Mana Pool") {
+            return 50000000;
+        }
+        if (attr1 == "Magic Find" && attr2 == "Veteran") {
+            return 50000000;
+        }
+    }
+    return 0;
+}
+
+function uniqueEquipmentAttr(Part, attr1, lvl1, attr2, lvl2) {
+    let bonusLvl1 = lvl1 == 6 ? 0.5 : Math.pow(2, 5-lvl1);
+    let bonusLvl2 = lvl2 == 6 ? 0.5 : Math.pow(2, 5-lvl2);
+
+    let prices = [1000000];
+    if (attr1 == "Mana Pool" || attr2 == "Mana Pool") {
+        prices.push((Part == "Necklace" ? 15 : Part == "Cloak" ? 18 : Part == "Belt" ? 12 : 11) / (attr1 == "Mana Pool" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Dominance" || attr2 == "Dominance") {
+        prices.push((Part == "Necklace" ? 14 : Part == "Cloak" ? 6 : Part == "Belt" ? 6 : 6) / (attr1 == "Dominance" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Lifeline" || attr2 == "Lifeline") {
+        prices.push((Part == "Necklace" ? 6 : Part == "Cloak" ? 20 : Part == "Belt" ? 24 : 26) / (attr1 == "Lifeline" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Veteran" || attr2 == "Veteran") {
+        prices.push((Part == "Necklace" ? 9 : Part == "Cloak" ? 9 : Part == "Belt" ? 9 : 6) / (attr1 == "Veteran" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Vitality" || attr2 == "Vitality") {
+        prices.push((Part == "Necklace" ? 4 : Part == "Cloak" ? 4 : Part == "Belt" ? 4 : 4) / (attr1 == "Vitality" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Magic Find" || attr2 == "Magic Find") {
+        prices.push((Part == "Necklace" ? 4 : Part == "Cloak" ? 20 : Part == "Belt" ? 14 : 14) / (attr1 == "Magic Find" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Mana Regeneration" || attr2 == "Mana Regeneration") {
+        prices.push((Part == "Necklace" ? 7 : Part == "Cloak" ? 5 : Part == "Belt" ? 2 : 4) / (attr1 == "Mana Regeneration" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Breeze" || attr2 == "Breeze") {
+        prices.push((Part == "Necklace" ? 10 : Part == "Cloak" ? 10 : Part == "Belt" ? 5 : 2) / (attr1 == "Breeze" ? bonusLvl1 : bonusLvl2));
+    }
+    if (attr1 == "Speed" || attr2 == "Speed") {
+        prices.push((Part == "Necklace" ? 6 : Part == "Cloak" ? 6 : Part == "Belt" ? 7 : 8) / (attr1 == "Speed" ? bonusLvl1 : bonusLvl2));
+    }
+
+    return Math.max(...prices);
+}
+
 
 EndedKuudra = 1;
 
@@ -160,6 +233,7 @@ armorParts = ["Helmet", "Chestplate", "Leggings", "Boots"];
 armorAttr = ["Arachno Resistance", "Blazing Resistance", "Breeze", "Dominance","Ender Resistance","Experience","Fortitude","Life Regeneration",
     "Lifeline","Magic Find","Mana Pool","Mana Regeneration","Speed","Undead Resistance","Veteran", "Vitality"];
 bookEnchants = ["Fatal Tempo", "Inferno", "Hardened Mana", "Ferocious Mana", "Strong Mana", "Mana Vampire"];
+equipmentParts = ["Belt", "Necklace", "Cloak", "Bracelet"];
 
 const romanVal = {
     I: 1,
@@ -212,8 +286,8 @@ register("tick", () => {
                 for (i = 0; i < 54; i++) {
                     // if there's an item
                     if (items[i] != null) {
-                        // Armor Managment part
 
+                        // Armor Management part
                         // try every combination of kuudra armor and piece on the item
                         armorTypes.forEach(armorType => {
                             armorParts.forEach(armorPart => {
@@ -221,7 +295,7 @@ register("tick", () => {
                                 if (items[i].getName().includes(armorType + " " + armorPart)) {
                                     spaces = 0;
                                     // sneaky move to not get stats as attributes (like Speed and Health Regen)
-                                    attributes = []
+                                    attributes = [];
                                     items[i].getLore().forEach(lorePart => {
                                         if (lorePart.includes("⚔")) {
                                             spaces += 1;
@@ -238,19 +312,47 @@ register("tick", () => {
                                         }
                                     });
                                     kuudraChestMoney += armorType + " " + armorPart + " : " + attributes[0][0] + attributes[0][1] + " and " + attributes[1][0] + attributes[1][1] +
-                                                            " => " + getPrice(armorType, armorPart, attributes[0][0], attributes[0][1], attributes[1][0], attributes[1][1]) + "\n";
-                                    setTimeout(function() {
-                                        kuudraChestMoney = "";
-                                    }, "5000");
+                                                            " => " + getArmorPrice(armorType, armorPart, attributes[0][0], attributes[0][1], attributes[1][0], attributes[1][1]) + "\n";
                                 }
                             });
                         });
+                        
+                        // Equipment Management part
+                        // try every combination of kuudra armor and piece on the item
+                        equipmentParts.forEach(equipmentPart => {
+                            // if there's a match, get the lore of the item
+                            if (items[i].getName().includes(equipmentPart)) {
+                                spaces = 0;
+                                // sneaky move to not get stats as attributes (like Speed and Health Regen)
+                                attributes = [];
+                                items[i].getLore().forEach(lorePart => {
 
+                                    if (lorePart.length < 5) {
+                                        spaces += 1;
+                                    }
+                                    // get the attributes and display them
+                                    if (spaces == 1) {
+                                        armorAttr.forEach(attr => {
+                                            if (lorePart.includes(attr) && !lorePart.includes("Grants")) {
+                                                splits = lorePart.split(' ');
+                                                level = romanToInt(splits[splits.length-1]);
+                                                attributes.push([attr, level]);
+                                            }
+                                        });
+                                    }
+                                });
+                                kuudraChestMoney += equipmentPart + " : " + attributes[0][0] + attributes[0][1] + " and " + attributes[1][0] + attributes[1][1] +
+                                                        " => " + getEquipmentPrice(equipmentPart, attributes[0][0], attributes[0][1], attributes[1][0], attributes[1][1]) + "\n";
+                            }
+                        })
 
-                        // Book Managment part
+                        // Book Management part
                     }
                 }
-                
+                setTimeout(function() {
+                    kuudraChestMoney = "";
+                }, "10000");
+
                 EndedKuudra = 0;
             }
         }
